3ed3a0728d68551d507aa2a25279827f864b37f5
牧言 <326132874@qq.com>
2018-12-18 20:30:07 +0800

    前端工作人员

--- src/main/java/com/itheima/gittest.java
+++ src/main/java/com/itheima/gittest.java
@@ -3,4 +3,8 @@ package com.itheima;
 public class gittest {
     private String username;
     private Integer age;
+
+    public static void main(String[] args) {
+        System.out.println("前端工作人员");
+    }
 }
